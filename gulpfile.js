9c6272b3378b74c31b6071707b26b04d1e49419f
Ben Papillon <bpapillon@umbel.com>
2015-03-16 14:02:22 -0500

    adding node js to jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
-		$ = require('gulp-load-plugins')(),
-		minifyCss = require('gulp-minify-css'),
-		paths = {
-			css: 'public/css/*.css',
-			css_dist: 'dict/*.css',
-			dist: 'public/dist/',
-			js: 'public/js/*.js'
-		};
+    $ = require('gulp-load-plugins')(),
+    minifyCss = require('gulp-minify-css'),
+    paths = {
+      css: 'public/css/*.css',
+      dist: 'public/dist/',
+      js: ['public/js/*.js', 'app.js', 'lib/*.js'],
+      js_public: 'public/js/*.js'
+    };
 
 gulp.task('css', function () {
   return gulp.src(paths.css)
@@ -18,7 +18,7 @@ gulp.task('css', function () {
 });
 
 gulp.task('js', function () {
-  return gulp.src(paths.js)
+  return gulp.src(paths.js_public)
     .pipe($.uglify())
     .pipe($.concat('showlist.min.js'))
     .pipe(gulp.dest(paths.dist));
@@ -31,8 +31,10 @@ gulp.task('jshint', function () {
 });
 
 gulp.task('watch', function() {
-	gulp.watch(paths.css, ['css'])
-	gulp.watch(paths.js, ['jshint', 'js'])
+  gulp.watch(paths.css_main, ['css:main'])
+  gulp.watch(paths.css_print, ['css:print'])
+  gulp.watch(paths.js, ['jshint'])
+  gulp.watch(paths.js_public, ['js'])
 });
 
 gulp.task('build', ['js', 'css']);
